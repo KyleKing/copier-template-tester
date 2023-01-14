27911c9f7b377dd260dac090ea9c5bd0140a6a0e
Kyle King <KyleKing@users.noreply.github.com>
2023-01-14 12:25:27 -0500

    build: copier update

--- tests/test_zz_copier_template_tester.py
+++ tests/test_zz_copier_template_tester.py
@@ -7,7 +7,7 @@ from copier_template_tester import __version__
 try:
     import tomllib
 except ModuleNotFoundError:
-    import tomli as tomllib  # ignore: type[no-redef]
+    import tomli as tomllib  # type: ignore[no-redef]
 
 
 def test_version():
