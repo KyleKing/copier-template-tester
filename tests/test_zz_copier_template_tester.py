b8d8ec2382fa59792638a11463fd4447c01da892
Kyle King <KyleKing@users.noreply.github.com>
2023-02-17 08:33:20 -0500

    build: copier update

--- tests/test_zz_copier_template_tester.py
+++ tests/test_zz_copier_template_tester.py
@@ -7,12 +7,12 @@ from copier_template_tester import __version__
 try:
     import tomllib
 except ModuleNotFoundError:
-    import tomli as tomllib  # ignore: type[no-redef]
+    import tomli as tomllib  # type: ignore[no-redef]
 
 
 def test_version():
     """Check that PyProject and __version__ are equivalent."""
-    data = Path('pyproject.toml').read_text()
+    data = Path('pyproject.toml').read_text(encoding='utf-8')
 
     result = tomllib.loads(data)['tool']['poetry']['version']
 
