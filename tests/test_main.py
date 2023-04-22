ba169719d8a8b923bc6b74a44edd031e2f65a0eb
Kyle King <KyleKing@users.noreply.github.com>
2023-04-22 00:16:51 -0400

    test: support temporary git directory for testing

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,12 +4,14 @@ from pathlib import Path
 import copier
 import pytest
 from beartype import beartype
+from corallium.log import get_logger
 
 from copier_template_tester.main import _validate_config, run
 
 from .configuration import TEST_DATA_DIR
+from .helpers import DEMO_DIR, add_commit, run_ctt, temporary_git_dir
 
-CTT_CMD = ['poetry', 'run', 'ctt']
+logger = get_logger()
 
 
 @pytest.mark.parametrize(
@@ -41,22 +43,45 @@ def test_main_with_copier_mock(monkeypatch):
 
     monkeypatch.setattr(copier, 'run_auto', _run_auto)
 
-    run(base_dir=TEST_DATA_DIR / 'copier_demo')
+    run(base_dir=DEMO_DIR)
 
 
 def test_main(shell):
-    ret = shell.run(*CTT_CMD, cwd=TEST_DATA_DIR / 'copier_demo')
+    ret = run_ctt(shell, cwd=DEMO_DIR)
 
     assert ret.returncode == 0
-    ret.stdout.matcher.fnmatch_lines_random(['*Creating:*copier_demo*no_all*'])
-    ret.stderr.matcher.fnmatch_lines_random([
+    ret.stdout.matcher.fnmatch_lines(['*Creating:*copier_demo*no_all*', ''])
+    # Check output from copier
+    ret.stderr.matcher.fnmatch_lines([
         '*Copying from template*',
-        '*.copier-answers.yml*',
+        '* .copier-answers.yml*',
+        '*identical* README.md*',
+        '*identical* script.py*',
+    ])
+
+
+def test_untracked_files(shell):
+    untracked_file = Path('template_dir/untracked_file.txt')
+    with temporary_git_dir(shell, DEMO_DIR) as copier_dir:
+        first_pass = run_ctt(shell, cwd=TEST_DATA_DIR, args=[f'--base-dir={copier_dir}'])
+        assert first_pass.returncode == 0
+
+        (copier_dir / untracked_file).write_text('Placeholder\n')
+        add_commit(shell, cwd=copier_dir)
+        ret = run_ctt(shell, cwd=TEST_DATA_DIR, args=[f'--base-dir={copier_dir}', '--check-untracked'])
+
+    assert ret.returncode == 1
+    ret.stdout.matcher.fnmatch_lines(['*Creating:*copier_demo*no_all*', ''])
+    # Check output from copier
+    ret.stderr.matcher.fnmatch_lines([
+        '*Copying from template*',
+        '*conflict* .copier-answers.yml*',
+        f'*create* {untracked_file.name}*',
     ])
 
 
 def test_main_missing_config(shell):
-    ret = shell.run(*CTT_CMD, cwd=TEST_DATA_DIR)
+    ret = run_ctt(shell, cwd=TEST_DATA_DIR)
 
     assert ret.returncode == 1
-    ret.stderr.matcher.fnmatch_lines_random(['*No configuration file found. Expected: *ctt.toml*'])
+    ret.stderr.matcher.fnmatch_lines(['*No configuration file found. Expected: *ctt.toml*'])
