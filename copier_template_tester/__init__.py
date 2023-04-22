66f383b4941d8527785b90c94351d920ee7f4ec4
Kyle King <KyleKing@users.noreply.github.com>
2023-04-22 03:09:15 -0400

    bump: version 1.1.0 → 1.2.0rc0

--- copier_template_tester/__init__.py
+++ copier_template_tester/__init__.py
@@ -1,6 +1,6 @@
 """copier_template_tester."""
 
-__version__ = '1.1.0'
+__version__ = '1.2.0rc0'
 __pkg_name__ = 'copier_template_tester'
 
 # ====== Above is the recommended code from calcipy_template and may be updated on new releases ======
