bb1fd30574b14d93583d682d239ab9b617e60811
Kyle King <KyleKing@users.noreply.github.com>
2023-04-21 21:57:24 -0400

    fix(#3): return error in pre-commit on new output directories (#5)
    Co-authored-by: Jairo Llopis <973709+yajo@users.noreply.github.com>

--- copier_template_tester/main.py
+++ copier_template_tester/main.py
@@ -4,7 +4,10 @@ Based on: https://github.com/copier-org/copier/blob/ccfbc9a923f4228af7ca2bf06749
 
 """
 
+import shlex
 import shutil
+import subprocess
+import sys
 from pathlib import Path
 
 import copier
@@ -56,6 +59,22 @@ def _render(  # type: ignore[no-untyped-def]
 
 
 @beartype
+def _ls_untracked_dir() -> set[Path]:
+    cmd = 'git ls-files --directory --exclude-standard --no-empty-dir --others'
+    process = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE)  # noqa: S603
+    stdout, _stderr = process.communicate()
+    return {Path.cwd() / _d.strip() for _d in stdout.decode().split('\n') if _d}
+
+
+@beartype
+def _check_for_untracked(output_paths: set[Path]) -> None:
+    """Resolves the edge case in #3 by raising when pre-commit won't error."""
+    if new_dirs := output_paths.intersection(_ls_untracked_dir()):
+        print(f'pre-commit error: untracked files from {new_dirs} must be added')  # noqa: T201
+        sys.exit(1)
+
+
+@beartype
 def run(base_dir: Path | None = None) -> None:
     """Main class to run ctt."""
     base_dir = base_dir or Path.cwd()
@@ -63,11 +82,14 @@ def run(base_dir: Path | None = None) -> None:
     defaults = config.get('defaults', {})
 
     input_path = base_dir
+    paths = set()
     for key, data in config['output'].items():
         output_path = base_dir / key
         output_path.mkdir(parents=True, exist_ok=True)
+        paths.add(output_path)
         print(f'Creating: {output_path}')  # noqa: T201
         _render(input_path, base_dir / output_path, data=defaults | data)
+    _check_for_untracked(paths)
 
 
 if __name__ == '__main__':  # pragma: no cover
